c67923b521cd0b61574e3afec8d8dcb6c3e41fe6
tiberiuioan35@gmail.com <tiberiuioan35@gmail.com>
2024-02-28 20:56:52 +0200

    Add resize method

--- lib/chess_position_detection.dart
+++ lib/chess_position_detection.dart
@@ -1,11 +1,31 @@
 import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
 import 'package:http/http.dart' as http;
+import 'package:image/image.dart';
 
 class ChessPositionDetection {
   Future<String> analyseImage(String imagePath) async {
+    final File imageFile = File(imagePath);
+
+    if (!imageFile.existsSync()) {
+      throw Exception('Image file does not exist');
+    }
+
+    final Uint8List imageBytes = await File(imagePath).readAsBytes();
+    final Image? image = decodeImage(imageBytes);
+
+    if (image == null) {
+      throw Exception('Failed to decode image');
+    }
+
+    final Image resizedImage = resizeImage(image, maxDimension: 700);
+    final Uint8List resizedImageBytes = encodePng(resizedImage);
+
     var request = http.MultipartRequest(
         'POST', Uri.parse('http://192.168.82.107:8080/api/get_chess_position'));
-    request.files.add(await http.MultipartFile.fromPath('image', imagePath));
+    request.files.add(http.MultipartFile.fromBytes('image', resizedImageBytes,
+        filename: 'snapshot.png'));
 
     var response = await request.send();
     if (response.statusCode == 200) {
@@ -16,5 +36,24 @@ class ChessPositionDetection {
     } else {
       throw Exception('Failed to upload image');
     }
+  }
+
+  Image resizeImage(Image image, {int maxDimension = 700}) {
+    int width = image.width;
+    int height = image.height;
+    int minDimension = width < height ? width : height;
+
+    if (minDimension > maxDimension) {
+      if (width > height) {
+        width = maxDimension;
+        height = (maxDimension * image.height / image.width).round();
+      } else {
+        height = maxDimension;
+        width = (maxDimension * image.width / image.height).round();
+      }
+      image = copyResize(image, width: width, height: height);
+    }
+
+    return image;
   }
 }
